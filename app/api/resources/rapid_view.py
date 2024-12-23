e6ebe53ac28bfcb1dc195b8d2d7a4a082c5bf462
jyothishnt <joyjoy.nt@gmail.com>
2024-12-23 13:59:12 +0000

    more tests

--- app/api/resources/rapid_view.py
+++ app/api/resources/rapid_view.py
@@ -19,56 +19,62 @@ router = APIRouter()
 @router.get("/{species_url_name}/{subpath:path}", name="Rapid Species Resources")
 async def resolve_species(
     request: Request, species_url_name: str, subpath: str = "", r: str = Query(None)
-):
+) -> RedirectResponse:
     assembly_accession_id = format_assembly_accession(species_url_name)
 
     if assembly_accession_id is None:
         raise HTTPException(
             status_code=422, detail="Unable to process input accession ID"
         )
+    try:
+        genome_object = get_genome_id_from_assembly_accession_id(assembly_accession_id)
 
-    genome_object = get_genome_id_from_assembly_accession_id(assembly_accession_id)
+        if genome_object and genome_object != {}:
+            genome_id = genome_object["genome_tag"] or genome_object["genome_uuid"]
 
-    if genome_object:
-        genome_id = genome_object["genome_tag"] or genome_object["genome_uuid"]
+            # Extract specific parameters because Ensembl url uses ; instead of &
+            query_string = request.scope["query_string"].decode()
+            query_params = parse_qs(query_string, separator=";")
 
-        # Extract specific parameters because Ensembl url uses ; instead of &
-        query_string = request.scope["query_string"].decode()
-        query_params = parse_qs(query_string, separator=";")
-
-        location = query_params.get("r", [None])[0]
-        gene_id = query_params.get("g", [None])[0]
-
-        if subpath == "" or re.search("Info/Index", subpath, re.IGNORECASE):
-            url = f"{ENSEMBL_URL}/species/{genome_id}"
-        elif re.search("Location", subpath):
-            url = f"{ENSEMBL_URL}/genome-browser/{genome_id}?focus=location:{location}"
-        elif re.search("Gene", subpath):
-            if re.search("Gene/Compara_Homolog", subpath):
-                url = (
-                    f"{ENSEMBL_URL}/entity-viewer/{genome_id}/gene:{gene_id}"
-                    f"?view=homology"
-                )
-            else:
-                url = f"{ENSEMBL_URL}/entity-viewer/{genome_id}/gene:{gene_id}"
-
-        elif re.search("Transcript", subpath):
-            if re.search("Domains|ProteinSummary", subpath):
-                url = (
-                    f"{ENSEMBL_URL}/entity-viewer/{genome_id}/gene:{gene_id}"
-                    f"?view=protein"
-                )
-            else:
-                url = f"{ENSEMBL_URL}/entity-viewer/{genome_id}/gene:{gene_id}"
+            url = construct_url(genome_id, subpath, query_params)
+            return RedirectResponse(url)
         else:
-            url = ENSEMBL_URL
+            raise HTTPException(status_code=404, detail="Genome not found")
 
-        return RedirectResponse(url)
+    except HTTPException as e:
+        logging.debug(e)
+        raise HTTPException(
+            status_code=e.status_code, detail="Unexpected error occured"
+        )
 
-    else:
-        raise HTTPException(status_code=404, detail="Genome not found")
+    except Exception as e:
+        logging.debug(f"Unexpected error occurred: {e}")
+        raise HTTPException(status_code=500, detail="Unexpected error occurred")
 
 
 @router.get("/", name="Rapid Home")
 async def resolve_home(request: Request):
     return RedirectResponse(ENSEMBL_URL)
+
+
+def construct_url(genome_id, subpath, query_params):
+    location = query_params.get("r", [None])[0]
+    gene_id = query_params.get("g", [None])[0]
+
+    if subpath == "" or re.search("Info/Index", subpath, re.IGNORECASE):
+        return f"{ENSEMBL_URL}/species/{genome_id}"
+    elif re.search("Location", subpath):
+        return f"{ENSEMBL_URL}/genome-browser/{genome_id}?focus=location:{location}"
+    elif re.search("Gene", subpath):
+        if re.search("Gene/Compara_Homolog", subpath):
+            return (
+                f"{ENSEMBL_URL}/entity-viewer/{genome_id}/gene:{gene_id}?view=homology"
+            )
+        return f"{ENSEMBL_URL}/entity-viewer/{genome_id}/gene:{gene_id}"
+    elif re.search("Transcript", subpath):
+        if re.search("Domains|ProteinSummary", subpath):
+            return (
+                f"{ENSEMBL_URL}/entity-viewer/{genome_id}/gene:{gene_id}?view=protein"
+            )
+        return f"{ENSEMBL_URL}/entity-viewer/{genome_id}/gene:{gene_id}"
+    return ENSEMBL_URL
--- app/tests/test_rapid.py
+++ app/tests/test_rapid.py
@@ -32,19 +32,16 @@ class TestRapid(unittest.TestCase):
             "genome2": f"{ENSEMBL_URL}/species/xyz",
         }
 
+    # Test species home page
     @patch("api.resources.rapid_view.get_genome_id_from_assembly_accession_id")
-    @patch("api.resources.rapid_view.format_assembly_accession")
     def test_rapid_species_home_success(
-        self,
-        mock_format_assembly_accession,
-        mock_get_genome_id_from_assembly_accession_id,
+        self, mock_get_genome_id_from_assembly_accession_id
     ):
 
         # test web-metadata-api response without genome_tag
         mock_get_genome_id_from_assembly_accession_id.return_value = (
             self.mock_genome_id_response1
         )
-        mock_format_assembly_accession.return_value = self.mock_ncbi_accession
 
         response = self.client.get(
             f"{self.mock_rapid_api_url}/{self.species_url_name}/",
@@ -72,3 +69,89 @@ class TestRapid(unittest.TestCase):
         self.assertEqual(
             response.headers["location"], self.mock_resolved_url["genome2"]
         )
+
+    # Test Region in detail page
+    @patch("api.resources.rapid_view.get_genome_id_from_assembly_accession_id")
+    def test_rapid_species_location_success(
+        self, mock_get_genome_id_from_assembly_accession_id
+    ):
+
+        # test web-metadata-api response without genome_tag
+        mock_get_genome_id_from_assembly_accession_id.return_value = (
+            self.mock_genome_id_response1
+        )
+
+        response = self.client.get(
+            f"{self.mock_rapid_api_url}/{self.species_url_name}/Location/View",
+            params={"r": "1:1000-2000"},
+            follow_redirects=False,
+        )
+
+        self.assertEqual(response.status_code, 307)  # Redirect
+        self.assertIn(
+            f"{ENSEMBL_URL}/genome-browser/genome_uuid1?focus=location:1:1000-2000",
+            response.headers["location"],
+        )
+
+    # Test Gene pages
+    @patch("api.resources.rapid_view.get_genome_id_from_assembly_accession_id")
+    def test_rapid_species_gene_compara_homolog(
+        self, mock_get_genome_id_from_assembly_accession_id
+    ):
+        mock_get_genome_id_from_assembly_accession_id.return_value = (
+            self.mock_genome_id_response1
+        )
+
+        response = self.client.get(
+            f"{self.mock_rapid_api_url}/{self.species_url_name}/Gene/Compara_Homolog",
+            params={"g": "GENE123"},
+            follow_redirects=False,
+        )
+
+        self.assertEqual(response.status_code, 307)  # Redirect
+        self.assertIn(
+            f"{ENSEMBL_URL}/entity-viewer/genome_uuid1/gene:GENE123?view=homology",
+            response.headers["location"],
+        )
+
+    # Test 404
+    @patch("api.resources.rapid_view.get_genome_id_from_assembly_accession_id")
+    def test_rapid_species_404_not_found(
+        self, mock_get_genome_id_from_assembly_accession_id
+    ):
+        mock_get_genome_id_from_assembly_accession_id.return_value = {}
+
+        response = self.client.get(
+            f"{self.mock_rapid_api_url}/{self.species_url_name}/Invalid_GCA"
+        )
+        self.assertEqual(response.status_code, 404)
+
+    # Test invalid url entity
+    def test_rapid_species_422_unprocessable_entity(self):
+        response = self.client.get(f"{self.mock_rapid_api_url}/Invalid_Name/")
+        self.assertEqual(response.status_code, 422)
+
+    # Test POST
+    def test_rapid_species_post_method_not_allowed(self):
+        response = self.client.post(
+            f"{self.mock_rapid_api_url}/{self.species_url_name}/"
+        )
+        self.assertEqual(response.status_code, 405)
+
+    # Test 500
+    @patch("api.resources.rapid_view.get_genome_id_from_assembly_accession_id")
+    def test_rapid_species_500_internal_server_error(
+        self, mock_get_genome_id_from_assembly_accession_id
+    ):
+        mock_get_genome_id_from_assembly_accession_id.return_value = (
+            self.mock_genome_id_response1
+        )
+
+        mock_get_genome_id_from_assembly_accession_id.side_effect = Exception(
+            "Unexpected error"
+        )
+
+        response = self.client.get(
+            f"{self.mock_rapid_api_url}/{self.species_url_name}/"
+        )
+        self.assertEqual(response.status_code, 500)
